4a1cf7d5840d308e17ee767b24bfc549b99ccbeb
Alyssa Jurgens <aljurgens@yahoo.com>
2019-08-16 10:27:46 -0600

    Create operate function that takes an operator

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ function multiply(n, m) {
 function divide(n, m) {
   return n / m;
 }
+
+function operate(op, n, m) {
+  switch(op) {
+    case '+':
+      return add(n, m);
+      break;
+    case '-':
+      return sub(n, m);
+      break;
+    case '*':
+      return multiply(n, m);
+      break;
+    case '/':
+      return divide(n, m);
+      break;
+    default:
+      return error;
+      break;
+  }
+}
