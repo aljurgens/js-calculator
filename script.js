fb54d5a8e9c4980e6f7c4f62d74879eeff38dd33
Alyssa Jurgens <aljurgens@yahoo.com>
2019-08-18 13:36:17 -0600

    Improve calculator functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const keys = document.querySelectorAll('.key');
-const reset = document.querySelector('.reset');
 const result = document.querySelector('.result');
 const expression = document.querySelector('.expression');
 const links = document.querySelector('.links');
 const linker = document.querySelector('.linker');
+let lastSubmitted = false;
 
 function add(n, m) {
   return n + m;
@@ -53,32 +53,89 @@ function clear(e) {
   expression.textContent = '';
 }
 
-function calculate(key) {
+function calculate(str) {
+  if(str.includes('(')) {
+    let start = str.indexOf('(') + 1;
+    let end = str.lastIndexOf(')');
+    let innerExpression = str.substring(start, end);
+    let calculatedExpression = calculate(innerExpression);
+    let newStr = str.replace(innerExpression, calculatedExpression);
+    if (newStr.indexOf('(') != 0) {
+      newStr = newStr.replace('(', '*');
+    } else {
+      newStr = newStr.replace('(', '');
+    }
+    if (newStr.lastIndexOf(')') != newStr.length - 1) {
+      newStr = newStr.replace(')', '*');
+    } else {
+      newStr = newStr.replace(')', '');
+    }
+    return calculate(newStr);
+  } else if(str.includes('+') || str.includes('-')) {
+    let index = null;
+    if(str.includes('+')) {
+      index = str.indexOf('+');
+    } else {
+      index = str.indexOf('-');
+    }
+    let op = str.charAt(index);
+    let firstExpression = str.substring(0, index);
+    let secondExpression = str.substring(index + 1, str.length);
+    if (firstExpression[0] == '*') {
+      firstExpression = firstExpression.substring(1, firstExpression.length);
+    }
+    if (firstExpression[firstExpression.length - 1] == '*') {
+      firstExpression = firstExpression.substring(0, firstExpression.length - 1);
+    }
+    if (secondExpression[0] == '*') {
+      secondExpression = secondExpression.substring(1, secondExpression.length);
+    }
+    if (secondExpression[secondExpression.length - 1] == '*') {
+      secondExpression = secondExpression.substring(0, secondExpression.length - 1);
+    }
+    firstExpression = calculate(firstExpression);
+    secondExpression = calculate(secondExpression);
+    return operate(op, firstExpression, secondExpression);
+  } else if(str.includes('*') || str.includes('/') || str.includes('%')) {
+    let index = 0;
+    if(str.includes('*')) {
+      index = str.lastIndexOf('*');
+    }
+    if(str.includes('/')) {
+      if (index < str.lastIndexOf('/')) {
+        index = str.lastIndexOf('/');
+      }
+    }
+    if (str.includes('%')){
+      if (index < str.lastIndexOf('%')) {
+        index = str.lastIndexOf('%');
+      }
+    }
+    let op = str.charAt(index);
+
+    let firstExpression = calculate(str.substring(0, index));
+    let secondExpression = calculate(str.substring(index + 1, str.length));
+    return operate(op, firstExpression, secondExpression);
+  } else {
+    return Number(str);
+  }
+}
+
+function computeExpression(key) {
   const keyNum = key.path['0'].innerText;
+  if (lastSubmitted) {
+    expression.textContent = '';
+    lastSubmitted = false;
+  }
   switch(keyNum) {
     case '=':
-
-      let exp = expression.textContent;
-      let op = null;
-      let x = 0;
-
-      for (let i = 0; i < exp.length-1; i++) {
-        if (exp[i] == '-' || exp[i] == '+' || exp[i] == '/' || exp[i] == '*' || exp[i] == '%') {
-          op = exp.charAt(i);
-          x = i;
-        }
-      }
-
-      let n = Number(expression.textContent.substring(0, x));
-      let m = Number(expression.textContent.substring(x + 1, expression.textContent.length));
-
-      let results = operate(op, n, m);
-      if (isNaN(results)) {
+      let results = parseFloat(calculate(expression.textContent).toFixed(2));
+      if (isNaN(results)){
         result.textContent = 'ERROR';
       } else {
         result.textContent = results;
       }
-
+      lastSubmitted = true;
       break;
     case 'AC':
       clear();
@@ -90,7 +147,7 @@ function calculate(key) {
   }
 }
 
-keys.forEach(key => key.addEventListener('click', calculate));
+keys.forEach(key => key.addEventListener('click', computeExpression));
 
 linker.addEventListener('mouseover', function(){
   links.classList.add('active');
